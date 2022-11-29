8e52419451c01d521cd275e1a545d473ea2fafdc
Adriel Santana <diel_san@hotmail.com>
2022-11-28 22:10:23 -0300

    criação da página start com o lifestatus animation

---
+++ projeto-react-native-obc/src/Components/Common/LifeStatus/index.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import Lottie from "lottie-react-native";
+
+const LifeStatus = () => {
+  return (
+    <View style={styles.container}>
+      <Lottie
+        source={require("../../../assets/education/education-100.json")}
+        autoPlay
+        loop
+        style={styles.educationAnimation}
+      />
+
+      <Lottie
+        source={require("../../../assets/money/money-100.json")}
+        autoPlay
+        loop
+        style={styles.moneyAnimation}
+      />
+
+      <Lottie
+        source={require("../../../assets/robot/robot-100-100.json")}
+        autoPlay
+        loop
+        style={styles.robotAnimation}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: 300,
+        height: 300,
+    },
+    robotAnimation:{
+        width: 190,
+        marginTop: 30,
+        marginLeft: 25,
+    },
+    educationAnimation:{
+        width: 100,
+        marginTop: 50,
+        marginLeft: 5,
+        position: 'absolute',
+    },
+    moneyAnimation:{
+        width: 100,
+        marginTop: 50,
+        marginLeft: 95,
+        position: 'absolute',
+    }
+})
+
+export default LifeStatus;
--- projeto-react-native-obc/src/pages/Start/index.jsx
+++ projeto-react-native-obc/src/pages/Start/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
 
+import LifeStatus from "../../Components/Common/LifeStatus";
+
 const Start = () => {
   return (
     <View style={styles.container}>
@@ -10,8 +12,9 @@ const Start = () => {
           source={require("../../assets/icons/logo3.png")}
           style={styles.logo}
           />
+          <LifeStatus/>
           <Text style={styles.description}>
-            Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o
+            Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o
             melhor nível.
           </Text>
         </View>
