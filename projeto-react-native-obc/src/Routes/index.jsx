2c2cdd36b7d1d38f07e4991caca88f696dbe0ccf
Adriel Santana <diel_san@hotmail.com>
2022-11-30 19:55:10 -0300

    renderização condicional caso ja tenha aberto o app

--- projeto-react-native-obc/src/Routes/index.jsx
+++ projeto-react-native-obc/src/Routes/index.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import AllPages from "./AllPages";
+import HomePage from "./HomePage";
 
 const Routes = () => {
-  return <AllPages />;
+  const [showHome, setShowHome] = useState(false);
+
+  return showHome ? <HomePage /> : <AllPages />;
 };
 
 export default Routes;
